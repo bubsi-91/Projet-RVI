457b7d8ae2eabe9591f00651322e5fe4a11ed6ae
corentin <corentinlesage02@gmail.com>
2022-01-31 16:18:56 +0100

    Add animation and path for character

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,22 +7,38 @@ public class PlayerController : MonoBehaviour
 {
     public NavMeshAgent agent;
     public Vector3 myVector;
-    public Transform point1;
-    public Transform point2;
-    public Transform point3;
+    public List<Transform> points;
+    public float d;
+    private Vector3 nextPosition;
+    public int nextIdPosition;
+
     // Start is called before the first frame update
     void Start()
     {
+        nextIdPosition = 0;
         agent = GetComponent<NavMeshAgent>();
-        agent.SetDestination(point1.position);
+        nextPosition = points[nextIdPosition].position;
+        agent.SetDestination(nextPosition);
+        
         
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if(Vector3.Distance(transform.position, point1.position) < 0.01f)
-            agent.SetDestination(point2.position);
-        agent.SetDestination(point2.position);
+    {  
+        d = Vector3.Distance(transform.position, nextPosition);
+        if (d < 2.5f) {
+            if (nextIdPosition < points.Count - 1)
+            {
+                nextIdPosition++;
+            }
+            else 
+            {
+                nextIdPosition = 0;
+            }
+            nextPosition = points[nextIdPosition].position;
+            agent.SetDestination(nextPosition);
+        }
+            
     }
 }
