387f5168ac7e14312adb61db01b4957b69b870d4
bubsi-91 <66141310+bubsi-91@users.noreply.github.com>
2022-02-17 23:56:22 +0100

    ecran de fin

--- Assets/Scripts/ExitTheBoat.cs
+++ Assets/Scripts/ExitTheBoat.cs
@@ -6,7 +6,7 @@ public class ExitTheBoat : MonoBehaviour
 
     void OnTriggerEnter(Collider other) {
         if (other.name == "XR Rig") {
-            SceneManager.LoadScene("boat");
+            SceneManager.LoadScene("EndGame");
         }
     }
 
